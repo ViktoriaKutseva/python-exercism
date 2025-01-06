866a115c3287fd8fa05480c4f85b33cd3c033f04
ViktoriaKutseva <kutsevaviktoria@gmail.com>
2025-01-06 16:30:31 +0600

    upd

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,2 +1,12 @@
-def leap_year(year):
-    pass
+# def leap_year(year):
+#     if year % 4 == 0 and not year % 100 == 0:
+#         return True
+#     elif year % 400 == 0:
+#         return True
+#     return False
+# print(leap_year(2024))
+# print(leap_year(2000))
+# print(leap_year(1000))
+palette_I = {'Grassy Green': '#9bc400', 'Purple Mountains Majesty': '#8076a3', 'Misty Mountain Pink': '#f9c5bd'}
+palette_I.get('Rock Brown', '#694605')
+print(palette_I)
