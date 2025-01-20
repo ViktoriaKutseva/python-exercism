e6dd40d3472d0aa45ae2a0c1c81a8fb61da10c1c
Viktoriia Kutseva <Kutsevaviktoriia@gmail.com>
2025-01-20 15:05:01 +0600

    upd

---
+++ python/locomotive-engineer/locomotive_engineer.py
@@ -0,0 +1,64 @@
+"""Functions which helps the locomotive engineer to keep track of the train."""
+
+
+def get_list_of_wagons(*args):
+    """Return a list of wagons.
+
+    :param: arbitrary number of wagons.
+    :return: list - list of wagons.
+    """
+    *list_of_wagons, = *args,
+    return list_of_wagons
+
+def fix_list_of_wagons(each_wagons_id, missing_wagons):
+    """Fix the list of wagons.
+
+    :param each_wagons_id: list - the list of wagons.
+    :param missing_wagons: list - the list of missing wagons.
+    :return: list - list of wagons.
+    """
+    wagon_1, wagon_2, wagon_3, *middle = each_wagons_id
+    *combined, = wagon_3, *missing_wagons, *middle, wagon_1, wagon_2
+    return combined
+
+
+def add_missing_stops(route: dict, **stops):
+    """Add missing stops to route dict.
+
+    :param route: dict - the dict of routing information.
+    :param: arbitrary number of stops.
+    :return: dict - updated route dictionary.
+    """
+    # list_of_stops = []
+    # for value in stops:
+    #     list_of_stops.append(stops[value])
+    # # all_stops = route, list_of_stops
+    route['stops'] = list(stops.values())
+    return route
+         
+
+
+def extend_route_information(route, more_route_information):
+    """Extend route information with more_route_information.
+
+    :param route: dict - the route information.
+    :param more_route_information: dict -  extra route information.
+    :return: dict - extended route information.
+    """
+    combined_route_info = {**route, **more_route_information}
+    return combined_route_info
+
+
+def fix_wagon_depot(wagons_rows: list):
+    """Fix the list of rows of wagons.
+
+    :param wagons_rows: list[list[tuple]] - the list of rows of wagons.
+    :return: list[list[tuple]] - list of rows of wagons.
+    """
+    result = [[] for _ in range(len(wagons_rows))]
+    for i in range(len(wagons_rows)):
+        for wagon in wagons_rows:
+            result[i].append(wagon[i])
+
+    return result
+
