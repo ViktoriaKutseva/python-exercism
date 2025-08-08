e39441c27295a0c9fbbe953f00f1d71b79df7b5c
ViktoriaKutseva <kutsevaviktoria@gmai.com>
2025-08-08 20:06:57 +0500

    upd

--- python/all-your-base/all_your_base.py
+++ python/all-your-base/all_your_base.py
@@ -1,2 +1,32 @@
-def rebase(input_base, digits, output_base):
-    pass
+def rebase(input_base, digits: list, output_base):
+
+    if input_base < 2:
+        raise ValueError('input base must be >= 2')
+    
+    if output_base < 2:
+        raise ValueError('output base must be >= 2')    
+    
+    for i in range(len(digits)):
+        print(digits[i])
+        if not input_base > digits[i] >= 0:
+            raise ValueError('all digits must satisfy 0 <= d < input base')
+    
+    len_of_number = len(digits)
+    result_in_10_base = 0
+    for i in range(len_of_number):
+        print(digits[i])
+        result_in_10_base += digits[i] * input_base ** (len_of_number - 1)
+        len_of_number = len_of_number - 1 
+    result = []
+    n = 0
+
+    while result_in_10_base >= output_base:
+        n =result_in_10_base - (result_in_10_base//output_base)*output_base
+        print(n)
+        result.append(n)
+        result_in_10_base = result_in_10_base//output_base
+    result.append(result_in_10_base)
+    result.reverse()
+    return result
+
+
