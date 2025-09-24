3fdf69e5aa938c315b38fc365b599f792989a42f
ViktoriaKutseva <kutsevaviktoria@gmail.com>
2025-09-25 00:04:24 +0500

    Implement word counting functionality in count_words function

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,9 @@
+import re 
+from collections import Counter
+
 def count_words(sentence):
-    pass
+    sentence = sentence.strip().strip('"\'').lower()
+    sentence = re.findall(r"[a-z0-9]+(?:'[a-z0-9]+)*", sentence)
+    counter = Counter(sentence) 
+    return counter
+
