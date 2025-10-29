4ff8cf3b761f3081c960d0aee45e56c9f3571b4d
ViktoriaKutseva <kutsevaviktoria@gmail.com>
2025-10-29 16:28:46 +0500

    Refactor School class to implement student addition and roster retrieval functionality

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,28 +1,38 @@
 class School:
     def __init__(self):
-        pass
-    def add_student(self, name, grade):
-        students = {}
-        self.students = students
-        self.name = name 
-        self.grade = grade
-        students[name] = grade
-        print(students)
-        result = []
-        result.append(name)
-        return result
+        self.roster_list = []
+        self.success = []
         
-
+    def add_student(self, name, grade):
+        names = [name_student for name_student, _ in self.roster_list]
+        if (name) not in names:
+            self.roster_list.append((name, grade))
+            self.success.append(True)
+        else:
+            self.success.append(False)
+        
     def roster(self):
-        pass
+        students = []
+        student_names = []
+        if self.roster_list:
+            for student in self.roster_list:
+                students.append(student)
+        students.sort(key=lambda t: (t[1], t[0]))
+        for name in students:
+            student_names.append(name[0])
+            
+        return student_names           
 
     def grade(self, grade_number):
-        self.grade_number = grade_number
-
+        students = []
+        for student in self.roster_list:
+            if grade_number == student[1]:
+                students.append(student)
+            else:
+                continue
+        students.sort(key=lambda t: (t[1], t[0]))
+        names = [name_student for name_student, _ in students]
+        return names 
+    
     def added(self):
-            return True
-        return False
-
-school = School()
-print(school.add_student('Aimee', 10))
-print(school.added())
+        return self.success
