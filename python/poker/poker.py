a176acdbde060351580013d16bdede0dd184b85d
ViktoriaKutseva <kutsevaviktoria@gmail.com>
2026-05-26 12:42:44 +0500

    feat(poker): enhance hand evaluation logic for poker

--- python/poker/poker.py
+++ python/poker/poker.py
@@ -1,11 +1,13 @@
-from collections import Counter
 
 def _is_straight(hand):
+    if hand == [2,3,4,5,14]:
+        return 2
     for i in range(1, len(hand)):
         if hand[i] == hand[i - 1] + 1:
             continue
         return False
-    return True
+    max_index = hand.index(max(hand))
+    return hand[max_index]
 
 def _is_flush(hand):
     for i in range(1, len(hand)):
@@ -43,9 +45,9 @@ def best_hands(hands):
     for hand in hands:
         hand_value_calc = 0
         sorted_by_value = []
-        hand = hand.split()
+        split_hand = hand.split()
 
-        for card in hand:
+        for card in split_hand:
             if len(card) == 2:
                 sorted_by_value.append(face_to_value[card[0]])
             else:
@@ -53,51 +55,39 @@ def best_hands(hands):
 
         sorted_by_value = sorted(sorted_by_value)
 
-        if _is_flush(hand) and _is_straight(sorted_by_value):
-            hand_value_calc += 1111 + sorted_by_value[-1]
+        if _is_flush(split_hand) and _is_straight(sorted_by_value):
+            hand_value_calc += 10000 + _is_straight(sorted_by_value)
 
         elif _is_foak(sorted_by_value):
-            hand_value_calc += 888 + _is_foak(sorted_by_value)
+            hand_value_calc += 9000 + _is_foak(sorted_by_value)*5 + sorted_by_value[-1]
             
         elif _is_thoak(sorted_by_value) and _is_toak(sorted_by_value):
-            hand_value_calc += 777 + (_is_thoak(sorted_by_value) * 2)
+            hand_value_calc += 8000 + (_is_thoak(sorted_by_value) * 2) + sum(_is_toak(sorted_by_value))
 
-        elif _is_flush(hand):
-            hand_value_calc += 666 + sorted_by_value[-1]
+        elif _is_flush(split_hand):
+            hand_value_calc += 7000 + sum(sorted_by_value)
 
         elif _is_straight(sorted_by_value):
-            hand_value_calc += 555 + sorted_by_value[-1]
+            hand_value_calc += 6000 + _is_straight(sorted_by_value)
 
         elif _is_thoak(sorted_by_value):
-            hand_value_calc += 444 + _is_thoak(sorted_by_value)
+            hand_value_calc += 5000 + _is_thoak(sorted_by_value)*5 + sorted_by_value[0] + sorted_by_value[1]*2
 
         elif _is_toak(sorted_by_value):
             pairs = _is_toak(sorted_by_value)
+            left_value = [card for card in sorted_by_value if card not in pairs]
             if len(pairs) == 2:
-                hand_value_calc += 333 + pairs[-1]*2 + pairs[0]
+                hand_value_calc += 4000 + pairs[-1]*5 + pairs[0] + sum(left_value)
             else:
-                hand_value_calc += 222 + pairs[0]
-                 
+                hand_value_calc += 3000 + pairs[-1]*5 + sum(left_value)
         else:
-            hand_value_calc += sorted_by_value[-1]
+            hand_value_calc += sorted_by_value[-1]*50 + sorted_by_value[-2]*10 + sorted_by_value[-3]*5 + sorted_by_value[-4]*2 + sorted_by_value[-5]
 
         hands_values.append(hand_value_calc)
-        max_index = hands_values.index(max(hands_values))
-        print(hands_values)
-    return [hands[max_index]]
-print(best_hands(["KC AH AS AD AC", "10C JC QC KC AC"]))
-
-# print(_is_toak([2,2,3,3,4]))
-
-## points for the cards : 
-# royal_flush = 1111
-# straight_flush = 999 + highest_card_value
-# four_of_a_kind = 888 + card_four_value
-# full_house = 777 + three_card_valuex2 + two_cards_value
-# flush = 666 + highest_value_card
-# straight = 555 + highest_value_card
-# three = 444 + highest_value_card
-# two_pairs = 333 + highest_value_1 + highest_value_2
-# pair = 222 + highest_pair_value
-# high_card = high_card
-
+        winner = []
+    print(hands_values)
+    for i, value in enumerate(hands_values):
+        if value == max(hands_values):
+            winner.append(hands[i])
+        continue
+    return winner
