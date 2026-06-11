70ee34a1d70c8a657a83873dd6c6f9e3bed69e1e
ViktoriaKutseva <kutsevaviktoria@gmail.com>
2026-06-11 16:01:09 +0500

    feat(clock): implement clock functionality and string representation
    feat(rectangles): add rectangles exercise with tests and documentation
    
    feat(simple-linked-list): create linked list implementation with tests

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,18 +1,26 @@
 class Clock:
     def __init__(self, hour, minute):
-        pass
+        total_minutes = hour * 60 + minute
+        self.hour = (total_minutes // 60) % 24
+        self.minute = total_minutes % 60
 
     def __repr__(self):
-        pass
+        return f"Clock({self.hour}, {self.minute})" 
 
     def __str__(self):
-        pass
+        hour = str(self.hour)
+        minute = str(self.minute)
+        if len(hour) == 1:
+            hour = "".join(["0", hour])
+        if len(minute) == 1:
+            minute = "".join(["0", minute])
+        return f"{hour}:{minute}"
 
     def __eq__(self, other):
-        pass
+        return self.hour == other.hour and self.minute == other.minute
 
     def __add__(self, minutes):
-        pass
-
+        return Clock(self.hour, self.minute + minutes)
+    
     def __sub__(self, minutes):
-        pass
+        return Clock(self.hour, self.minute - minutes)
---
+++ python/simple-linked-list/simple_linked_list.py
@@ -0,0 +1,36 @@
+class EmptyListException(Exception):
+    pass
+
+
+class Node:
+    def __init__(self, value):
+        self.value = self.value
+
+    def value(self):
+        pass
+
+    def next(self):
+        pass
+
+
+class LinkedList:
+    def __init__(self, values=None):
+        self.values = values
+
+    def __iter__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def head(self):
+        pass
+
+    def push(self, value):
+        pass
+
+    def pop(self):
+        pass
+
+    def reversed(self):
+        pass
