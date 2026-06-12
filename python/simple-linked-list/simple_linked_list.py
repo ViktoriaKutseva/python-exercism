5a657854c3cabee4ac6a76ec1fdda76f40064c45
Viktoriia Kutseva <kutsevaviktoria@gmail.com>
2026-06-12 19:59:46 +0500

    feat(linked-list): implement basic linked list structure and functionality

--- python/simple-linked-list/simple_linked_list.py
+++ python/simple-linked-list/simple_linked_list.py
@@ -1,36 +1,54 @@
-class EmptyListException(Exception):
-    pass
 
+class EmptyListException(Exception):                                                                                              
+    def __init__(self, message):                                                                                                  
+        self.message = message                                                                                                    
+        super().__init__(message)   
 
 class Node:
     def __init__(self, value):
-        self.value = self.value
-
+        self._value = value
+        self._next = None
     def value(self):
-        pass
+        return self._value
 
     def next(self):
-        pass
+        return self._next
 
 
 class LinkedList:
     def __init__(self, values=None):
-        self.values = values
+        self._head = None
+        self._len = 0
+        if values is not None:
+            for value in values:
+                self.push(value)
 
     def __iter__(self):
-        pass
+        current = self._head
+        while current is not None:
+            yield current._value 
+            current = current._next
 
     def __len__(self):
-        pass
+        return self._len
 
-    def head(self):
-        pass
+    def head(self):                                                                                                                   
+        if self._head is None:                                                                                                        
+            raise EmptyListException("The list is empty.")                                                                            
+        return self._head     
 
     def push(self, value):
-        pass
-
+        new_node = Node(value)
+        new_node._next = self._head
+        self._head = new_node
+        self._len += 1
     def pop(self):
-        pass
+        if self._head is None:
+            raise EmptyListException("The list is empty.")
+        value = self._head._value 
+        self._head = self._head._next
+        self._len -= 1
+        return value
 
     def reversed(self):
-        pass
+        return LinkedList(list(self))
